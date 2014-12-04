e6badb552886722b872a8ddf116a8c76697c5d59
Robert <robzhu@gmail.com>
2014-12-04 14:38:59 -0500

    NowinServerFactory now reading server port from '--server.urls' key

--- src/Nowin.vNext/NowinServerFactory.cs
+++ src/Nowin.vNext/NowinServerFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
@@ -6,30 +7,41 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting.Server;
 using Microsoft.AspNet.Owin;
 using Microsoft.Framework.ConfigurationModel;
- 
+
 namespace Nowin.vNext
 {
     public class NowinServerFactory : IServerFactory
     {
         private Func<object, Task> _callback;
+        private const string ServerUrlConfigKey = "server.urls";
+        private const int DefaultPort = 5000;
 
-        private Task HandleRequest(IDictionary<string, object> env)
+        private Task HandleRequest( IDictionary<string, object> env )
         {
-            return _callback(new OwinFeatureCollection(env));
+            return _callback( new OwinFeatureCollection( env ) );
         }
 
-        public IServerInformation Initialize(IConfiguration configuration)
+        public IServerInformation Initialize( IConfiguration configuration )
         {
-            // TODO: Parse config
+            int port = DefaultPort;
+            try
+            {
+                //expect a commandline argument of the format: "--server.urls http://localhost:5000"
+                var portString = configuration[ ServerUrlConfigKey ].Split( ':' ).Last();
+                port = int.Parse( portString );
+            }
+            catch { }
+
+            Console.WriteLine( "Hosting on port: {0}", port );
             var builder = ServerBuilder.New()
-                                       .SetAddress(IPAddress.Any)
-                                       .SetPort(5000)
-                                       .SetOwinApp(OwinWebSocketAcceptAdapter.AdaptWebSockets(HandleRequest));
+                                       .SetAddress( IPAddress.Any )
+                                       .SetPort( port )
+                                       .SetOwinApp( OwinWebSocketAcceptAdapter.AdaptWebSockets( HandleRequest ) );
 
-            return new NowinServerInformation(builder);
+            return new NowinServerInformation( builder );
         }
 
-        public IDisposable Start(IServerInformation serverInformation, Func<object, Task> application)
+        public IDisposable Start( IServerInformation serverInformation, Func<object, Task> application )
         {
             var information = (NowinServerInformation)serverInformation;
             _callback = application;
@@ -40,20 +52,13 @@ namespace Nowin.vNext
 
         private class NowinServerInformation : IServerInformation
         {
-            public NowinServerInformation(ServerBuilder builder)
+            public NowinServerInformation( ServerBuilder builder )
             {
                 Builder = builder;
             }
 
             public ServerBuilder Builder { get; private set; }
-
-            public string Name
-            {
-                get
-                {
-                    return "Nowin";
-                }
-            }
+            public string Name { get { return "Nowin"; } }
         }
     }
 }
